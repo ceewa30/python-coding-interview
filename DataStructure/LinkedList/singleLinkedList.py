3f9bf88e2299c12824919fc096445e351f6c5096
ceewa30 <ceewa30@gmail.com>
2023-11-22 12:24:17 -0500

    Creation of LinkedList

--- DataStructure/LinkedList/singleLinkedList.py
+++ DataStructure/LinkedList/singleLinkedList.py
@@ -5,4 +5,13 @@ class Node:
         self.value = value
         self.next = None
 
-        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+new_linked_list = LinkedList(10)
+print(new_linked_list)
+
