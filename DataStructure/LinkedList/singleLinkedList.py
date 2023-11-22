2b6c02691e29b3fad2c822df76ee1fef558bae17
ceewa30 <ceewa30@gmail.com>
2023-11-22 13:20:29 -0500

    Inserting the value at the beginning

--- DataStructure/LinkedList/singleLinkedList.py
+++ DataStructure/LinkedList/singleLinkedList.py
@@ -43,11 +43,24 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
+# Inserting the value at the beginning of the Linked List
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
 
 
 
 new_linked_list = LinkedList()
 new_linked_list.append(10)
 new_linked_list.append(20)
+print(new_linked_list)
+new_linked_list.prepend(40)
 print(new_linked_list)
 
